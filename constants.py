2f10402df1218dd6ab9090b87d4d7ad25216138d
Teymuraz <timamail2@gmail.com>
2024-02-16 23:09:44 +0400

    this is constants which we are used in slugify.py project

---
+++ constants.py
@@ -0,0 +1,26 @@
+from string import punctuation
+
+
+__all__ = (
+    "SYMBOLS",
+    "AZ_TO_EN_LETTERS"
+)
+
+
+SYMBOLS = punctuation
+
+AZ_TO_EN_LETTERS = {
+    "ə": "e",
+    "ç": "c",
+    "ş": "s",
+    "ğ": "g",
+    "ö": "o",
+    "ü": "u",
+    "ı": "i",
+}
+
+# COLORS_DATA = {
+#     "red": "0[31m]/"
+# }
+
+# COLORS_DATA["red"]
